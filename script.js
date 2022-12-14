0b413e06179502739c0b6c842caa9d62dbc3509c
Gabrielle Carolyne Da Silveira <111080270+Gabrielle-Carolyne@users.noreply.github.com>
2022-12-14 18:53:43 -0300

    game

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const qiqi = document.querySelector('.qiqi');
-const obstaculo = document.querySelector ('.obstaculo');
-
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 const jump = () => {
-qiqi.classList.add ('jump');
-
-setTimeout(() => {
-    qiqi.classList.remove('jump');
-}, 500);
+    mario.classList.add('jump');
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    },500);
 }
+const loop = setInterval (()=> {
 
-const loop = setInterval(() => {
+const pipePosition = pipe.offsetLeft;
+const marioPosition = window.getComputedStyle(mario).bottom.replace('px','');
 
-const obstaculoPosition = obstaculo .offsetleft;
+console.log (marioPosition)
 
-if (obstaculoposition ≤ 20 ) {
-
-obstaculo .style.animation = 'none';
+if (pipePosition <= 120 &&  pipePosition > 0 && marioPosition <80) {
+    pipe.style.animation = 'none'; 
+    pipe.style.left = `${pipePosition}px`;
+}
+},10);
+document.addEventListener('keydown', jump);
